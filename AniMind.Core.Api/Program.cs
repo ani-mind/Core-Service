fbe5ae3cfe130d5615e2d94b7883360944974b18
Egor Verbilo <verbiloyegor@gmail.com>
2026-05-30 16:52:23 +0300

    chore: add void solution

---
+++ AniMind.Core.Api/Program.cs
@@ -0,0 +1,28 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    
+    app.MapScalarApiReference(options =>
+    {
+        options.Theme = ScalarTheme.Mars;
+        
+        options.WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
